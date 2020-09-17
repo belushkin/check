76158ddec3f22ee8bee9b1f6db7a4f41aa2a4ad5
Maxim Bielushkin <5299918-belushkin@users.noreply.gitlab.com>
2020-09-17 11:57:13 +0300

    Added composer

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-http://0.0.0.0:8000/
-http://0.0.0.0:8080/
+docker-compose up
+docker-compose exec check composer install
+
+http://0.0.0.0:8000/ - main application
+http://0.0.0.0:8080/ - adminer
+
+
+
--- public/index.php
+++ public/index.php
@@ -1,28 +1,23 @@
 <?php declare(strict_types=1);
 
-use App\DB\DBFactory;
-use App\Http\Router;
-use App\Http\Request;
-use App\Persistence\CSV;
-use App\View\View;
-use App\Transformer\JSONTransformer;
-use App\Aggregate\OnBoardingFlow;
+use App\Service\Http\Router;
+use App\Service\Http\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
-$router = new Router(new Request());
-
-$router->get('/', function() {
-    $tpl = new View( dirname(__DIR__).'/src/Template' );
-
-    return $tpl->render( 'home');
-});
-
-$router->get('/api/v1/data', function() {
-    $db = DBFactory::create(
-        new CSV(dirname(__DIR__).'/data/export.csv')
-    );
-
-    $aggregator = new OnBoardingFlow($db->export());
-    return JSONTransformer::fromArray($aggregator->aggregate());
-});
+//$router = new Router(new Request());
+//
+//$router->get('/', function() {
+//    $tpl = new View( dirname(__DIR__).'/src/Template' );
+//
+//    return $tpl->render( 'home');
+//});
+//
+//$router->get('/api/v1/data', function() {
+//    $db = DBFactory::create(
+//        new CSV(dirname(__DIR__).'/data/export.csv')
+//    );
+//
+//    $aggregator = new OnBoardingFlow($db->export());
+//    return JSONTransformer::fromArray($aggregator->aggregate());
+//});
